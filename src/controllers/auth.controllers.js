8b6733670bb2f852fe237749c7c34543a59b7b4b
RAAVI BALA SIVA DURGA PRASAD <84697652+RBSDP@users.noreply.github.com>
2025-07-02 07:55:02 +0000

    updated register user logic in auth controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -2,23 +2,25 @@
 import {asyncHandler} from '../utils/async-handler.js'
 
 import { User } from '../models/user.models.js'
-
+import {ApiError} from '../utils/api-error.js'
+import {ApiResponse} from "../utils/api-responce.js"
 const registerUser = asyncHandler(async(req,res) => {
 
 
     const {email,password, username,role} = req.body
 
     
-    //checking user id db
+    // checking user id db
     try {
         const existinguser = await User.findOne({email})
         if(existinguser){
             throw new ApiError(422, "user already exists")
         }
+    }catch{
+        throw new ApiError(400, "error connecting to DB")
     }
 
-
-
+   
     //creating the user is db
 
     
@@ -26,11 +28,18 @@ const registerUser = asyncHandler(async(req,res) => {
         email,password,username
     })
 
-    if(!user){
-        res.status(400).json({
-            "message" : "unable to send message"
-        })
+    const createdUser = await User.findById(user._id).select("-password", "- refreshToken")
+
+    if(!createdUser) {
+        throw new ApiError(400, "erro while creattin user")
     }
+
+    res.status(201).json(
+
+        new ApiResponse(201,createdUser, "user registered successfully")
+    )
+
+
 
     //sending a email to the user 
 
