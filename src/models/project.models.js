d7d16d734f8c81c705d231e52f06ddffe4b6dcb8
RAAVI BALA SIVA DURGA PRASAD <84697652+RBSDP@users.noreply.github.com>
2025-07-02 08:05:20 +0000

    updated peoject schema

--- src/models/project.models.js
+++ src/models/project.models.js
@@ -1,6 +1,24 @@
 import mongoose, { Schema } from "mongoose";
+import { User } from "./user.models";
 
-const projectSchema = new Schema({})
+const projectSchema = new Schema({
+
+    title : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    description :{
+        type : String  
+    },
+    createdBy : {
+        type : Schema.Types.ObjectId,
+        ref:User,
+        required : true
+
+    }
+
+}, {timestamps : true})
 
 
 export const Project = mongoose.model("Project", projectSchema)
